2ff23d6d6e3fcf46e88fae9200864b9e37e27f66
ratuqra <qratu95@gmail.com>
2026-03-01 15:58:29 +0700

    Program SIMD (Numpy working)

---
+++ SIMD/simd_nilai.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print("SIMD - Tambah Nilai Siswa")
+
+nilai = np.array([70, 75, 80, 65, 90])
+
+hasil = nilai + 10
+
+print("Nilai lama:", nilai)
+print("Nilai setelah ditambah 10:", hasil)
